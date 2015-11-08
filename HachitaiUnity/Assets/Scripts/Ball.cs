8fdb5ea0febfe8a0d348255d1732689298ef7d3b
Kumajirou <you1120521@i.softbank.jp>
2015-11-08 18:01:39 +0900

    ボールがフレーム管理を行えます
    できた！

--- HachitaiUnity/Assets/Scripts/Ball.cs
+++ HachitaiUnity/Assets/Scripts/Ball.cs
@@ -6,21 +6,28 @@ public class Ball : MonoBehaviour {
 	private GameObject instanceBall;
 	private float randomX;
 	private float randomZ;
+	private float instanceFrame;
+	private float randomFrame;
 
 	// Use this for initialization
 	void Start () {
 		instanceBall = null;
 		randomX = 0f;
 		randomZ = 0f;
+		instanceFrame = 0f;
+		randomFrame = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown("n")){
+		instanceFrame++;
+		if(instanceFrame >= randomFrame){
 			instanceBall = (GameObject)Instantiate(ball, new Vector3(0, 0, 0), Quaternion.identity);
-			instanceBall.GetComponent<Rigidbody>().AddForce(new Vector3(randomX, 0, randomZ) * Time.deltaTime);
+			instanceBall.GetComponent<Rigidbody>().velocity = transform.forward;
 			randomX = Random.Range(-1, 1);
 			randomZ = Random.Range(-1, 1);
+			randomFrame = Random.Range(60f, 180f);
+			instanceFrame = 0f;
 		}
 	}
 	
